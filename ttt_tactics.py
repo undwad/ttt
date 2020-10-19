58b6f372fcbdbc7c85f119dce4c05181b897b637
yayanka <yayanka@MacBook-Pro-max.local>
2020-10-19 13:46:47 +0400

    +

--- ttt_tactics.py
+++ ttt_tactics.py
@@ -78,9 +78,9 @@ from tensorflow.data.experimental  import AUTOTUNE
 
 
 
-from time import *
+from time        import *
 
-
+from composition import *
 
 # %matplotlib inline
 
@@ -141,80 +141,6 @@ def throttle(interval):
         return Throttle(fn,interval)
 
     return decorator
-
-
-
-def compose(*ff):
-
-    def composition(*args,**kwargs):
-
-        for f in reversed(ff):
-
-            x              = f(*args,**kwargs)
-
-            is_args        = type(x) == Tuple
-
-            is_args_kwargs = is_args and len(x) == 2 and type(x[0]) == Tuple and type(x[1]) == Dict
-
-            if is_args_kwargs:  args,kwargs = x
-
-            elif is_args:       args,kwargs = x,{}
-
-            else:               args,kwargs = (x,),{}
-
-        return x
-
-    return composition
-
-
-
-class Composable:
-
-    def __init__(self,f):
-
-        self.f = f
-
-    def __call__(self,*args,**kwargs):
-
-        return self.f(*args,**kwargs)
-
-    def __lshift__(self,other):              
-
-        return Composable(compose(self,other))
-
-    def __rshift__(self,other):              
-
-        return Composable(compose(other,self))
-
-    def __and__(self,other):
-
-        def f(*args,**kwargs):
-
-            return self(*args,**kwargs) and other(*args,**kwargs)
-
-        return Composable(f)
-
-    def __or__(self,other):
-
-        def f(*args,**kwargs):
-
-            return self(*args,**kwargs) or other(*args,**kwargs)
-
-        return Composable(f)
-
-    def __invert__(self):
-
-        def f(*args,**kwargs):
-
-            return not self(*args,**kwargs)
-
-        return Composable(f)
-
-    
-
-def composable(f):
-
-    return Composable(f)
 
 
 
